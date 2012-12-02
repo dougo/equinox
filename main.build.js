ce59addea57e2fe4a52befee8d7a8f456a0ab539
Doug Orleans <dougorleans@gmail.com>
2012-12-01 19:53:54 -0500

    Add build file for the RequireJS optimizer.

---
+++ main.build.js
@@ -0,0 +1,23 @@
+// Use this for the RequireJS optimizer:
+//   node r.js -o main.build.js
+// This creates an optimized copy of the app in ../equinox-build.
+// For details, see:
+//   http://requirejs.org/docs/optimization.html
+//   http://requirejs.org/docs/jquery.html
+
+({
+  appDir: "./",
+  baseUrl: ".",
+  dir: "../equinox-build",
+
+  paths: {
+    // For jquery plugins that require the module ID "jquery".
+    jquery: "lib/require-jquery"
+  },
+
+  modules: [
+    {
+      name: "main"
+    }
+  ]
+})
