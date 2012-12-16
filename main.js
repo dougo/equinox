124887f037cddec32d8445215391ee50cdbcfffc
Doug Orleans <dougorleans@gmail.com>
2012-12-16 00:40:34 -0500

    Add jquerypp.  Also, cache busting.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-require(["lib/jquery.mobile"], function() {
+requirejs.config({
+  baseUrl: 'lib',
+  urlArgs: 'bust=' + new Date().getTime()
+});
+
+require(["jquery", "jquerypp/event/drag", "jquery.mobile"], function($) {
+  $('.hex').on('draginit', function (ev, drag) { });
   $('#main').show();
 });
